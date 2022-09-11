073fc565b6520b4c75cf1db4a91beb48105565a9
Matthew <mhornby123@gmail.com>
2022-09-11 15:18:49 +0100

    dynamic add station from map or copy the lat/lng + some ui

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -1,5 +1,7 @@
 "use strict";
 
+
+
 const map = {
   createMap(stations) {
     //Create Map object and set view to Ireland Center
@@ -28,6 +30,7 @@ const map = {
     function onMapClick(e) {
       clickMarker.remove(map);
       clickMarker = L.marker(e.latlng).addTo(map);
+      var testLat = e.latlng.lat
 
       geocodeService
         .reverse()
@@ -35,35 +38,31 @@ const map = {
         .run(function (error, result) {
           if (result.address.Neighborhood == "") {
             clickMarker.bindPopup(`
-          <div class="ui left labeled button" tabindex="0">
-          <a class="ui basic right pointing label">
-            <section>
-              <b>Lat</b>:${e.latlng.lat.toFixed(2)}, <b>Lng:</b>${e.latlng.lng.toFixed(2)}
-            </section>
-            </a>
-            <div class="ui mini blue button">
-              Copy
-            </div>
-          </div>`);
+            <div class="ui left labeled button" tabindex="0">
+              <a class="ui basic right pointing label">
+              <section>
+                <b>Lat</b>:${e.latlng.lat.toFixed(2)}, <b>Lng:</b>${e.latlng.lng.toFixed(2)}
+              </section>
+              </a>
+              <button class="ui mini blue button" onclick="copyLatLng(${e.latlng.lat.toFixed(2)},${e.latlng.lng.toFixed(2)})">
+                Copy
+              </button>
+            </div>`);
           } else {
             clickMarker.bindPopup(`
-          <div class="ui left labeled button" tabindex="0">
-            <a class="ui basic right pointing label">
+            <div class="ui left labeled button" tabindex="0">
+              <a class="ui basic right pointing label">
               <section>
                 ${result.address.Neighborhood}
               </section>
-            </a>
-            <div class="ui blue button">
-              <form action="/dashboard/addstation" method="post">
+              </a>
+              <form action="/dashboard/addstation" method="post" id="myForm">
                 <input type="hidden" value=${result.address.Neighborhood} name="name">
                 <input type="hidden" value=${e.latlng.lat.toFixed(2)} name="latitude">
                 <input type="hidden" value=${e.latlng.lng.toFixed(2)} name="longitude">
-              <button type="submit" class="btn-link">Add Station</button>
-            </form>
+              </form>
+              <button type="submit" form="myForm" class="ui blue button">Add Station</button>
             </div>
-
-            
-          </div>
           `);
           }
           clickMarker.openPopup();
@@ -73,20 +72,7 @@ const map = {
   },
 };
 
-//Add big red circle
-/*var circle = L.circle([51.508, -0.11], {
-      color: "red",
-      fillColor: "#f03",
-      fillOpacity: 0.5,
-      radius: 500,
-    }).addTo(map);*/
-
-//Add polygon shape
-/*var polygon = L.polygon([
-      [51.509, -0.08],
-      [51.503, -0.06],
-      [51.51, -0.047],
-    ]).addTo(map);*/
-
-//Adding popup Directly to Coord
-//var popup = L.popup().setLatLng([51.513, -0.09]).setContent("I am a standalone popup.").openOn(map);
+function copyLatLng(lat, lng) {
+  document.getElementById("stationLatitude").value = lat
+  document.getElementById("stationLongitude").value = lng
+}
